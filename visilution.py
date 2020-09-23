1d9763d73abb2df3e47b06dc98b4240d3aa9df7e
Satyam Krishna <63470846+satyam41@users.noreply.github.com>
2020-09-23 11:40:52 +0530

    Revert "Function"

--- visilution.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt, numpy as np
-city = ['Mumbai', 'Delhi', 'Jabalpur', 'Patna', 'Gaya']
-pop = [18400000, 19000000, 1270000, 2050000, 471000]
-plt.xlabel("City")
-plt.ylabel("Poplution of the City (in cores)")
-# plt.plot(city, pop, "r--")
-# plt.bar(city, pop, color=['r','k','g', 'b'], width=0.5)
-plt.pie(pop, labels=city, autopct="%1.1f%%")
-plt.show()
