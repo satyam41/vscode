37b98cd4d868afd3c7d581e74d3ddf0f7407ff2e
Satyam <thesatyam10@gmail.com>
2020-12-29 19:22:11 +0530

    change something

--- google.py
+++ google.py
@@ -10,17 +10,17 @@ def budget(my_budget,house_prices):
                 count_list = house_prices.count(house_prices[i])
             case+=1
             print("Case # :",case)
-                print("In your budget number of houses you buy", count_list)
+            print("In your budget number of houses you buy", count_list)
             # break
 
 
 test_cases = [] 
-num_test_cases = int(input())
+num_test_cases = int(input("Enter number of tests: "))
 while num_test_cases > 0: 
-	input_list = input().split(' ') 
+	input_list = input('Input list: ').split(' ') 
 	num_houses = int(input_list[0]) 
 	my_budget = int(input_list[1]) 
-	house_prices = input().split(' ') 
+	house_prices = input('input house price: ').split(' ') 
 	house_prices = [int(price) for price in house_prices] 
 	test_cases.append([my_budget, house_prices]) 
 	num_test_cases -= 1 
