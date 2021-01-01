2548abec49c8bb9b56b86eeab23b61b5762d1470
Satyam <thesatyam10@gmail.com>
2021-01-01 21:01:35 +0530

    Create a new files

---
+++ string.py
@@ -0,0 +1,17 @@
+a = "Satyam Krishna"
+# print(a.swapcase())
+
+b = "this is a normal laptop"
+# print(b.title())
+
+c = "this is a India"
+# print(a.istitle())
+
+d = "this is a game"
+# print(d.upper())
+
+e = "THIS IS SUMESH"
+# print(e.lower())
+
+f = "i love you aditi"
+print(f.title())
