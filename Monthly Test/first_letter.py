fab2cc10733b206dc3c1460e7ccf94e6cbfd2a9a
Satyam <thesatyam10@gmail.com>
2021-02-18 20:03:17 +0530

    modified

--- Monthly Test/first_letter.py
+++ Monthly Test/first_letter.py
@@ -4,6 +4,6 @@ def name_of_first_letter(name, surname):
     return firstchar, surfirstchar
 
 if __name__ == "__main__":
-    name = "Satyam"
-    surname = "Krishna"
+    name = input("Enter your first name: ")
+    surname = input("Enter your surname: ")
     print(f"Your first name and second name of first letter is {name_of_first_letter(name,surname)} and your name is {name} {surname}")
