c6542de2e251fb84fa4ea6e67de26d70a956ea73
Satyam Krishna <thesatyam10@gmail.com>
2021-06-07 10:20:43 +0530

    Display a string to the user

---
+++ displayString.py
@@ -0,0 +1,14 @@
+def Display(str): 
+     m="" 
+     for i in range(0,len(str)): 
+         if(str[i].isupper()): 
+             m=m+str[i].lower() 
+         elif str[i].islower(): 
+             m=m+str[i].upper() 
+         else: 
+             if i%2==0:
+                 m=m+str[i-1] 
+             else:
+                 m=m+"#"
+     print(m) 
+Display('Fun@Python3.0')
