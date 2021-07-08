0fb75ab4e5aaa4609ce764a0dc2cc524c2e77954
Satyam Krishna <thesatyam10@gmail.com>
2021-07-08 10:18:53 +0530

    This is the new mind reader game and this is better from last one

---
+++ mindGame2.py
@@ -0,0 +1,23 @@
+import random
+import time
+
+print("Choose the number in your mind........")
+time.sleep(3)
+
+print("Take the same number from your friend....")
+time.sleep(3)
+
+computerNumber = random.randint(1, 10)
+print("Take the computer number: ", computerNumber)
+time.sleep(3)
+
+print("Add all the number....")
+time.sleep(3)
+
+print("Half the number....")
+time.sleep(3)
+
+print("Return the number to your friend....")
+time.sleep(10)
+
+print("The remaining number is: ", computerNumber/2)
