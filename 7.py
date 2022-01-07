4fc36817b27a5da8893bcce510b0390cd0b9c190
Satyam Krishna <thesatyam10@gmail.com>
2022-01-07 19:59:56 +0530

    Tuples

---
+++ 7.py
@@ -0,0 +1,16 @@
+"""Tuple is also a type of data storage, It is use to store data and it is immutable means it not changeable.
+empty_tuple = ()
+non_empty_tuple = (1,2,3,4,5)
+non_empty_tuple_single_element = (1,)
+
+functions of tuple:
+    1.tuple.index()
+    2.tuple.count()
+
+tup = (1,2,3,2,4,5) # it cannot change.
+print(tup)
+
+print(tup.index(3))
+
+print(tup.count(2))
+"""
